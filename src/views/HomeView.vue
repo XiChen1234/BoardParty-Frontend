377d5ef3fc6c41a30331cc3a584910b6f2c61ca7
XiChen1234 <xichen888.private@gmail.com>
2026-04-02 09:39:54 +0800

    feat: delete base vue code
    删除基础vue代码，开始进行开发

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    BoardParty 首页
+  </div>
 </template>
